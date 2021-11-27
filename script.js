7b9d0a58349b4820dd894105aa54f4248b2a5fb1
geethu64 <geethu.g2010@gmail.com>
2021-11-27 20:40:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let displayValue=(num)=>result.value+=num
+// function displayValue(num){
+//     let txt_box=document.querySelector("#result")
+//     txt_box.value+=num
+
+// }
+let clearBox=()=>result.value=""
+// function clearBox(){
+//     document.querySelector("#result").value="";
+
+// }
+let evaluateExpression=()=>result.value=eval(result.value)
+// function evaluateExpression(){
+//     let txt_box=document.querySelector("#result");
+//     txt_box.value=eval(txt_box.value)
+
+
+// }
+let removalElement=()=>result.value=result.value.slice(0,-1)
+// function removalElement(){
+//     let txt_box=document.querySelector("#result");
+//     txt_box.value=txt_box.value.slice(0,-1)
+
+// }
